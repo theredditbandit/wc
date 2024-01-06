477b41ea1f7bbb25b1ed2b9e9afc73bd87b318a7
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-01-06 16:28:09 +0530

    modify wordCount implementation

--- wordCount.go
+++ wordCount.go
@@ -9,26 +9,22 @@ import (
 func getWordCount(f string) int {
 	ofile := openFile(f)
 	defer ofile.Close()
-	lc := 0
 	wc := 0
 	scanner := bufio.NewScanner(ofile)
 	for scanner.Scan() {
-		lc++
 		line := scanner.Text()
-		words := countWords(line)
+		words := getWords(line)
+        numOfWords := len(words)
 		if *debug {
-			fmt.Printf("words: %v\n", words)
+			fmt.Println(words, " -> ", numOfWords)
 			fmt.Println("--------")
 		}
-		wc += words
+		wc += numOfWords
 	}
 	return wc
 }
 
-func countWords(l string) int {
-    words := strings.Fields(l) // this is better than splitting the string on " "
-	if len(words) != 0 && *debug {
-		fmt.Println(words, " -> ", len(words))
-	}
-	return len(words)
+func getWords(l string) []string {
+	words := strings.Fields(l) // this is better than splitting the string on " "
+	return words
 }
